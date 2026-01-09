b1f999d48ce991d55f644fb88abb0d9b188e0926
Ruth Westnidge Brown <rhw@Simons-iMac.local>
2026-01-09 15:35:46 +0000

    Install Tailwind, link in HTML, import in index.css, add to vite config. Install react-router-dom, add BrowserRouter, Routes & Route with path and elements to complete routing. Add pages with placeholder texts. Set up directory structure with components, pages, data, hooks and utilities directories. Inital project set up.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Store } from "./pages/Store";
+import { About } from "./pages/About";
 
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Store />} />
+      <Route path="/" element={<About />} />
+    </Routes>
+  );
 }
 
 export default App;
