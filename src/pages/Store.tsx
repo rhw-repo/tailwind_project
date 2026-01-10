98c5d6e6119b2e9db064d50ce3ee1dee71a3753c
Ruth Westnidge Brown <rhw@Simons-iMac.local>
2026-01-10 20:29:31 +0000

    Comit map through objects from data/items.json: Add grid syntax to return. TODO: debug grid syntax, number of columns does not change across different devices.

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -4,6 +4,9 @@ export const Store = () => {
   return (
     <>
       <h1>Store</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {storeItems.map((item) => JSON.stringify(item))}
+      </div>
     </>
   );
 };
