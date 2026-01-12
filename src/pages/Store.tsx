20a0c1dbcf1e1f5a19004c972f82f0fc5025cde9
Ruth Westnidge Brown <rhw@Simons-iMac.local>
2026-01-12 15:30:14 +0000

    Wrap Store page in a section with max-width of 1024px, prevent excessively wide display making users turn head left to right in larger desktops. Add clamp rule for red bage in Navbar button prevent oversized in smaller devices

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -4,12 +4,14 @@ import { StoreItemCard } from "../components/StoreItemCard";
 export const Store: React.FC = () => {
   return (
     <>
-      <h1 className="text-4xl font-extrabold text-back-600">Store</h1>
-      <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 m-4">
-        {storeItems.map((item) => (
-          <StoreItemCard key={item.id} {...item} />
-        ))}
-      </div>
+      <section className="max-w-5xl">
+        <h1 className="text-4xl font-extrabold text-back-600">Store</h1>
+        <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 m-4">
+          {storeItems.map((item) => (
+            <StoreItemCard key={item.id} {...item} />
+          ))}
+        </div>
+      </section>
     </>
   );
 };
