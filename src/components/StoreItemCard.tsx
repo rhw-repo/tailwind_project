c6caa99c3cbcf9b543f9cc19543dce757151cbaa
Ruth Westnidge Brown <rhw@Simons-iMac.local>
2026-01-12 15:18:22 +0000

    Add buttons to card with placeholder for display of total items in cart. Add wrappers to allow for responsiveness of navbar. Add responsive classes to StoreItemCard elements. Styles template before adding functionality.

--- src/components/StoreItemCard.tsx
+++ src/components/StoreItemCard.tsx
@@ -19,12 +19,28 @@ export const StoreItemCard: React.FC<StoreItemCardProps> = ({
         className="w-full h-full object-cover rounded-sm"
       />
     </div>
-    <h2 className="font-extrabold text-2xl text-black-600 hover:text-gray-900 focus:ring-2 focus:ring-offset-2 focus:ring-blue-700 active:text-purple-950">
-      {name}
-    </h2>
-    <p className="text-lg font-semibold text-gray-800 hover:text-gray-900 focus:ring-2 focus:ring-offset-2 focus:ring-blue-700 active:text-purple-950">
-      &euro;{price}
-    </p>
+    <div className="flex justify-between">
+      <h2 className="font-extrabold text-2xl text-black-600 hover:text-gray-900 focus:ring-2 focus:ring-offset-2 focus:ring-blue-700 active:text-purple-950">
+        {name}
+      </h2>
+      <p className="text-lg font-semibold text-gray-800 hover:text-gray-900 focus:ring-2 focus:ring-offset-2 focus:ring-blue-700 active:text-purple-950">
+        &euro;{price}
+      </p>
+    </div>
+    <div className="flex justify-center gap-4">
+      <button className="bg-blue-700 text-neutral-100 rounded-sm px-4 py-2">
+        +
+      </button>
+      <span>no. in cart</span>
+      <button className="bg-blue-700 text-neutral-100 rounded-sm px-4 py-2">
+        -
+      </button>
+    </div>
+    <div className="flex justify-center">
+      <button className="bg-red-400 text-neutral-100 rounded-sm p-2 m-4">
+        Remove
+      </button>
+    </div>
   </div>
 );
 
--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -4,9 +4,7 @@ import { StoreItemCard } from "../components/StoreItemCard";
 export const Store: React.FC = () => {
   return (
     <>
-      <h1 className="self-start ml-64 text-4xl font-extrabold text-back-600">
-        Store
-      </h1>
+      <h1 className="text-4xl font-extrabold text-back-600">Store</h1>
       <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 m-4">
         {storeItems.map((item) => (
           <StoreItemCard key={item.id} {...item} />
