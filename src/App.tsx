005c23defd84d76f69a10382e6c1df91a97fffe8
Ruth Westnidge Brown <rhw@Simons-iMac.local>
2026-01-12 18:29:43 +0000

    Add context ShoppingCartContext with useContext() to manage context. Allows for increaseCartQuantity, decreaseCartQuantity, getItemQuanity and removeFromCart functions to be added to event listeners for buttons in StoreItemsCard. Runs in manual testing of those buttons.

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,21 @@ import { Home } from "./pages/Home";
 import { Store } from "./pages/Store";
 import { About } from "./pages/About";
 import { Navbar } from "./components/Navbar";
+import { ShoppingCartProvider } from "./context/ShoppingCartContext";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <main className="m-4 flex flex-col justify-center items-center">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/store" element={<Store />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </main>
+      <ShoppingCartProvider>
+        <Navbar />
+        <main className="m-4 flex flex-col justify-center items-center">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/store" element={<Store />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </main>
+      </ShoppingCartProvider>
     </>
   );
 }
--- src/components/StoreItemCard.tsx
+++ src/components/StoreItemCard.tsx
@@ -1,3 +1,4 @@
+import { useShoppingCart } from "../context/ShoppingCartContext";
 import { formatCurrency } from "../utilities/formatCurrency";
 
 export interface StoreItemCardProps {
@@ -7,53 +8,71 @@ export interface StoreItemCardProps {
   imgUrl: string;
 }
 
-//Test placeholder
-const quantity = 1;
-
 export const StoreItemCard: React.FC<StoreItemCardProps> = ({
   id,
   name,
   price,
   imgUrl,
-}) => (
-  <div key={id} className="shadow-sm p-4 rounded-sm h-full">
-    <div className="aspect-3/2 w-full">
-      <img
-        src={imgUrl}
-        alt={name}
-        className="w-full h-full object-cover rounded-sm"
-      />
-    </div>
-    <div className="flex justify-between items-baseline mb-8">
-      <h2 className="font-extrabold text-2xl text-black-600 hover:text-gray-900 focus:ring-2 focus:ring-offset-2 focus:ring-blue-700 active:text-purple-950">
-        {name}
-      </h2>
-      <p className="text-lg font-semibold text-gray-800 hover:text-gray-900 focus:ring-2 focus:ring-offset-2 focus:ring-blue-700 active:text-purple-950">
-        {formatCurrency(price)}
-      </p>
-    </div>
+}) => {
+  const {
+    getItemQuantity,
+    increaseCartQuantity,
+    decreaseCartQuantity,
+    removeFromCart,
+  } = useShoppingCart();
+  const quantity = getItemQuantity(id);
+  return (
+    <div key={id} className="shadow-sm p-4 rounded-sm h-full">
+      <div className="aspect-3/2 w-full">
+        <img
+          src={imgUrl}
+          alt={name}
+          className="w-full h-full object-cover rounded-sm"
+        />
+      </div>
+      <div className="flex justify-between items-baseline mb-8">
+        <h2 className="font-extrabold text-2xl text-black-600 hover:text-gray-900 focus:ring-2 focus:ring-offset-2 focus:ring-blue-700 active:text-purple-950">
+          {name}
+        </h2>
+        <p className="text-lg font-semibold text-gray-800 hover:text-gray-900 focus:ring-2 focus:ring-offset-2 focus:ring-blue-700 active:text-purple-950">
+          {formatCurrency(price)}
+        </p>
+      </div>
 
-    <div className="mt-auto">
-      {quantity === 0 ? (
-        <button className="w-full bg-blue-700 text-neutral-100 rounded-sm px-4 py-2">
-          Add To Cart
-        </button>
-      ) : (
-        <div className="flex flex-col items-center gap-2">
-          <div className="flex justify-center items-center gap-2">
-            <button className="bg-blue-700 text-neutral-100 rounded-sm px-4 py-2">
-              +
-            </button>
-            <span className="text-2xl">{quantity} in cart</span>
-            <button className="bg-blue-700 text-neutral-100 rounded-sm px-4 py-2">
-              -
+      <div className="mt-auto">
+        {quantity === 0 ? (
+          <button
+            onClick={() => increaseCartQuantity(id)}
+            className="w-full bg-blue-700 text-neutral-100 rounded-sm px-4 py-2"
+          >
+            Add To Cart
+          </button>
+        ) : (
+          <div className="flex flex-col items-center gap-2">
+            <div className="flex justify-center items-center gap-2">
+              <button
+                onClick={() => increaseCartQuantity(id)}
+                className="bg-blue-700 text-neutral-100 rounded-sm px-4 py-2"
+              >
+                +
+              </button>
+              <span className="text-2xl">{quantity} in cart</span>
+              <button
+                onClick={() => decreaseCartQuantity(id)}
+                className="bg-blue-700 text-neutral-100 rounded-sm px-4 py-2"
+              >
+                -
+              </button>
+            </div>
+            <button
+              onClick={() => removeFromCart(id)}
+              className="bg-red-400 text-xs sm:text-sm md:text-base lg:text-lg text-neutral-100 rounded-sm p-2 m-4"
+            >
+              Remove
             </button>
           </div>
-          <button className="bg-red-400 text-xs sm:text-sm md:text-base lg:text-lg text-neutral-100 rounded-sm p-2 m-4">
-            Remove
-          </button>
-        </div>
-      )}
+        )}
+      </div>
     </div>
-  </div>
-);
+  );
+};
