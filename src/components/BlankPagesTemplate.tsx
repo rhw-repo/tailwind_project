2955880fe6540e387bc4607d0639605598288c89
Ruth Westnidge Brown <rhw@Simons-iMac.local>
2026-01-16 14:59:25 +0000

    Add flex-1 to each element in landscape view to ensure uniform spacing. Prevent columns of unequal width and messy looking layout

--- src/components/BlankPagesTemplate.tsx
+++ src/components/BlankPagesTemplate.tsx
@@ -9,27 +9,27 @@ const BlankPagesTemplate: FC = () => {
   return (
     <section className="flex justify-center items-center h-screen w-screen bg-[url('/imgs/error-boundary-bg-image.svg')] bg-cover bg-center">
       <div className="flex flex-col justify-center items-center landscape:flex-row landscape:h-[80vh] landscape:lg:h-auto landscape:xl:flex-col w-3xl p-5 rounded-xl bg-gray-50 text-neutral-900 text-center text-balance text-2xl font-bold portrait:m-8 portrait:sm:md-4 m-4">
-        <div className="landscape:flex flex-col">
+        <div className="landscape:flex flex-col landscape:flex-1">
           <h1 className="text-5xl landscape:text-3xl md:text-9xl text-gray-600 m-4">
             Coming Soon
           </h1>
-          <h2 className="text-gray-500 text-base text-balance md:text-4xl landscape:text-xl">
+          <h2 className="text-gray-500 text-base text-balance md:text-4xl landscape:text-xl landscape:flex-1">
             This page has no content at this time.
           </h2>
         </div>
-        <div className="aspect-3/2 p-4 overflow-hidden mb-8 landscape:mb-0 landscape:w-2xs">
+        <div className="aspect-3/2 p-4 overflow-hidden mb-8 landscape:mb-0 landscape:w-2xs landscape-flex-1">
           <img
             src={errorBoundaryImg}
             alt="A retro style robot toy stares with a fixed expression."
             className="object-cover"
           />
         </div>
-        <div className="flex flex-col justify-between mt-4 gap-4">
+        <div className="flex flex-col justify-between mt-4 gap-4 landscape:flex-1">
           <p className="text-base landscape:text-base font-medium text-gray-500 text-balance">
             We apologize for the inconvenience.
           </p>
           <p className="text-base landscape:text-base font-medium text-gray-700 text-balance">
-            Please use the button below to go to the store.
+            Please visit the Store page.
           </p>
           <button
             className="py-2 px-4 bg-blue-700 text-neutral-50 landscape:text-sm border rounded cursor-pointer mb-4"
