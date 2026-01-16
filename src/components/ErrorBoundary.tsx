b213f3b3c7b0f3429e97b985d9a4160c41e3e3f6
Ruth Westnidge Brown <rhw@Simons-iMac.local>
2026-01-16 10:53:51 +0000

    Add styling suing Tailwind CSS landscape to display in row with text elements vertically stacked. Adjust font colors and sizing in all views and remove dev phase red border aound the content.

--- src/components/ErrorBoundary.tsx
+++ src/components/ErrorBoundary.tsx
@@ -1,7 +1,6 @@
-// Component must be imported to write any class component
+// 'Component' must be imported to write any class component
 import React, { Component, type ReactNode } from "react";
 import errorBoundaryImg from "/imgs/error-boundary-image.webp";
-//import errorBoundaryBgImage from "/imgs/error-boundary-bg-image.svg";
 
 interface Props {
   children: ReactNode;
@@ -27,7 +26,7 @@ class ErrorBoundary extends Component<Props, State> {
   }
 
   componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
-    // Log the error to your error reporting service
+    // Dev phase only. Production: log the error to an error reporting service
     console.error("Error caught by ErrorBoundary:", error, errorInfo);
   }
 
@@ -38,26 +37,33 @@ class ErrorBoundary extends Component<Props, State> {
     }
   }
 
+  // When error, display fallback UI with button to navigate to store page
   render() {
     if (this.state.hasError) {
       return (
         this.props.fallback || (
-          <section className="fixed inset-0 bg-[url('/imgs/error-boundary-bg-image.svg')] bg-cover bg-center flex justify-center items-center h-screen">
-            <div className="flex flex-col justify-center items-center w-3xl p-5 border-2 border-red-600 rounded-sm bg-gray-100 text-neutral-900 text-center text-balance text-2xl font-bold m-4">
-              <h1 className="text-7xl md:text-9xl text-gray-600 m-4">Oops!</h1>
-              <h2 className="text-3xl md;text-4xl">Something went wrong.</h2>
-              <div className="aspect-3/2 p-4 overflow-hidden mb-8">
+          <section className="bg-[url('/imgs/error-boundary-bg-image.svg')] bg-cover bg-center flex justify-center items-center h-screen">
+            <div className="flex flex-col justify-center items-center landscape:flex-row landscape:h-[80vh] landscape:lg:h-auto landscape:xl:flex-col w-3xl p-5 rounded-xl bg-gray-50 text-neutral-900 text-center text-balance text-2xl font-bold portrait:m-8 portrait:sm:md-4 m-4">
+              <div className="landscape:flex flex-col">
+                <h1 className="text-7xl landscape:text-6xl md:text-9xl text-gray-600 m-4">
+                  Oops!
+                </h1>
+                <h2 className="text-gray-500 text-3xl md:text-4xl landscape:text-2xl">
+                  Something went wrong.
+                </h2>
+              </div>
+              <div className="aspect-3/2 p-4 overflow-hidden mb-8 landscape:mb-0">
                 <img
                   src={errorBoundaryImg}
-                  alt="A retro style robot toy straes with a fixed expression."
-                  className="aspect object-cover"
+                  alt="A retro style robot toy stares with a fixed expression."
+                  className="object-cover"
                 />
               </div>
-              <p className="text-2xl font-medium text-gray-500 text-balance">
-                We apologize for the inconvenience. Please use the button below
-                to go back.
-              </p>
-              <div className="flex justify-between mt-4 gap-4">
+              <div className="flex flex-col justify-between mt-4 gap-4">
+                <p className="text-2xl landscape:text-base font-medium text-gray-500 text-balance">
+                  We apologize for the inconvenience. Please use the button
+                  below to go back.
+                </p>
                 <button
                   className="py-2 px-4 bg-blue-700 text-neutral-50 border rounded cursor-pointer"
                   onClick={() => (window.location.href = "/store")}
